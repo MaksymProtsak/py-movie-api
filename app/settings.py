42c952017fa0e4fee7d0364d82118f7b6708b360
Maksym Protsak <112944888+MaksymProtsak@users.noreply.github.com>
2024-10-31 12:22:49 +0200

    Replased secret key for os.environ.get.

--- app/settings.py
+++ app/settings.py
@@ -9,8 +9,10 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0bdjwe*prygdc38=uzu0656+u3e0cx0w4uw@m^9%yr$e!eqc5e'
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "default not secure key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
