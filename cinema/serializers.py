bdf803b60202d9e3d3b606f4671e86620aab2a4f
Maksym Protsak <112944888+MaksymProtsak@users.noreply.github.com>
2024-10-31 08:50:20 +0200

    Renamed class name from CinemaSerializer to MovieSerializer. Changed model name from Cinema to Movie.

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -1,15 +1,15 @@
 from rest_framework import serializers
-from cinema.models import Cinema
+from cinema.models import Movie
 
 
-class CinemaSerializer(serializers.Serializer):
+class MovieSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     title = serializers.CharField(max_length=255, required=True)
-    description = serializers.CharField(blank=True)
+    description = serializers.CharField(required=False)
     duration = serializers.IntegerField(required=True)
 
     def create(self, validated_data):
-        return Cinema.objects.create(**validated_data)
+        return Movie.objects.create(**validated_data)
 
     def update(self, instance, validated_data):
         instance.title = validated_data.get("title", instance.title)
